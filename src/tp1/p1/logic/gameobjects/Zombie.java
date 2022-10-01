4ada2656ee1bdfe90dc9034cbbdc3c74d543e9c3
Z-T-H <112755085+ZTH7@users.noreply.github.com>
2022-10-01 09:49:09 +0200

    Update Zombie.java

--- src/tp1/p1/logic/gameobjects/Zombie.java
+++ src/tp1/p1/logic/gameobjects/Zombie.java
@@ -2,6 +2,25 @@ package tp1.p1.logic.gameobjects;
 
 public class Zombie {
 	
+	private int vida;
+        private int col;
+        private int row;
 	
-	
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public boolean attack(){
+        
+        return false;
+    }
 }
